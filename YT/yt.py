ddf61dca2ac8bb7e559b3125a0bccd00aeb1d262
CK88924 <kevin.chen.880924@gmail.com>
2022-07-10 08:46:58 +0800

    下載縮圖
    新增下載縮圖

--- YT/yt.py
+++ YT/yt.py
@@ -5,6 +5,7 @@ Created on Tue Jul  5 01:12:45 2022
 @author: asus
 """
 from pytube import Playlist, YouTube
+import urllib.request as url_req
 
 def Down_YT():
     yt_input = input('請輸入影片網址:\n')
@@ -13,6 +14,7 @@ def Down_YT():
         progMP4 = yt.streams.filter(progressive=True, file_extension='mp4')# 篩選 progressive 類型的 MP4 影片格式
         targetMP4 = progMP4.order_by('resolution').desc().first()# 找出解析度最好的 MP4 影片
         video_file = targetMP4.download('video')
+        print('影片下載完成')
     except:
         pass
 
@@ -25,16 +27,36 @@ def Down_List():
             progMP4 = yt.streams.filter(progressive=True, file_extension='mp4')# 篩選 progressive 類型的 MP4 影片格式
             targetMP4 = progMP4.order_by('resolution').desc().first()# 找出解析度最好的 MP4 影片
             video_file = targetMP4.download('videos')
+        print('播放清單下載完畢')
     except:
         pass
     
+def Down_Image():
+    yt_input = input('請輸入影片網址:\n')
+    try:
+         yt = YouTube(yt_input)
+         yt_image_url = yt.thumbnail_url
+         filename = 'image//'+ yt.title +'.jpg'
+         try:
+             url_req.urlretrieve(yt_image_url,filename)
+             print('圖片下載完成')
+         except:
+             pass
+         
+    except:
+        pass
+    
+    
+    
 if __name__ == '__main__':
     while True:
-        act = input('請選擇模式:1(下載單支youtube) 2(下載清單)')
+        act = input('請選擇模式:1(下載單支youtube) 2(下載清單) 3(下載縮圖)')
         if act == '1':
             Down_YT()
         elif act == '2':
             Down_List()
+        elif act == '3':
+            Down_Image()
         else:
             break
     
