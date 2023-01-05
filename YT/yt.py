53a27d5eb09d146d3215daa6fe1e38cf1e965e8b
CK88924 <48667814+CK88924@users.noreply.github.com>
2023-01-06 01:13:53 +0800

    Update yt.py

--- YT/yt.py
+++ YT/yt.py
@@ -5,6 +5,7 @@ Created on Tue Jul  5 01:12:45 2022
 @author: asus
 """
 from pytube import Playlist, YouTube
+from datetime import datetime
 import urllib.request as url_req
 import  os
 
@@ -20,28 +21,49 @@ def Down_YT():
             try:
                 mp3 = yt.streams.filter().get_audio_only().download(filename= 'mp3\\'+ yt.title + '.mp3')
                 print('mp3下載完成')
+                
             except OSError:
-                  mp3 = yt.streams.filter().get_audio_only().download(filename= 'mp3\\temp.mp3')
+                  now = datetime.now() # current date and time
+                  temp_time = now.strftime("%Y_%m_%d%H_%M_%S")
+                  temp_filename = 'mp3//' + 'temp_'+temp_time +'.mp3'
+                  mp3 = yt.streams.filter().get_audio_only().download(filename=temp_filename)
                   print("temp download ok")
-            finally:
+                  
+            except Exception as e:
+                print(e)
                 pass
+          
             
            
            
         elif ask == 'v':
-             video_file = targetMP4.download('video')
-             print('影片下載完成')
+            try:
+                video_file = targetMP4.download('video')
+                print('影片下載完成')
+                
+            except Exception as e:
+                print(e)
+                pass
+             
         elif ask == 'a':
-              mp3 = yt.streams.filter().get_audio_only().download(filename= 'mp3\\'+ yt.title + '.mp3')
-              video_file = targetMP4.download('video')
-              print('影片及mp3下載完成')
+            try:
+                mp3 = yt.streams.filter().get_audio_only().download(filename= 'mp3\\'+ yt.title + '.mp3')
+                video_file = targetMP4.download('video')
+                print('影片及mp3下載完成')
+                
+            except Exception as e:
+                print(e)
+                pass
+            
         
        
-    except:
+    except Exception as e:
+        print(e)
         pass
 
 def Down_List():
     list_input = input('請輸入播放清單ex:https://www.youtube.com/playlist?list=\n')
+    list_input = 'https://www.youtube.com/playlist?list=' + list_input
     
     
     try:
@@ -50,30 +72,48 @@ def Down_List():
         for video in range(len(pl)): 
             yt = YouTube(pl[video])
             if ask == 'm':
-                mp3 = yt.streams.filter(only_audio=True).first()
-                mp3 = mp3.download(output_path='mp3')
-                base, ext = os.path.splitext(mp3)
-                new_file = base + '.mp3'
-                os.rename(mp3, new_file)
+               try:
+                   mp3 = yt.streams.filter(only_audio=True).first()
+                   mp3 = mp3.download(output_path='mp3')
+                   base, ext = os.path.splitext(mp3)
+                   new_file = base + '.mp3'
+                   os.rename(mp3, new_file)
+                   
+               except Exception as e:
+                   print(e)
+                   pass
                 
             elif ask =='v':
-                progMP4 = yt.streams.filter(progressive=True, file_extension='mp4')# 篩選 progressive 類型的 MP4 影片格式
-                targetMP4 = progMP4.order_by('resolution').desc().first()# 找出解析度最好的 MP4 影片
-                targetMP4.download('videos')
+                try:
+                    progMP4 = yt.streams.filter(progressive=True, file_extension='mp4')# 篩選 progressive 類型的 MP4 影片格式
+                    targetMP4 = progMP4.order_by('resolution').desc().first()# 找出解析度最好的 MP4 影片
+                    targetMP4.download('videos')
+                    
+                except Exception as e:
+                    print(e)
+                    pass
+               
                 
             elif ask =='a':
-                mp3 = yt.streams.filter(only_audio=True).first()
-                mp3 = mp3.download(output_path='mp3')
-                base, ext = os.path.splitext(mp3)
-                new_file = base + '.mp3'
-                os.rename(mp3, new_file)
+                try:
+                    mp3 = yt.streams.filter(only_audio=True).first()
+                    mp3 = mp3.download(output_path='mp3')
+                    base, ext = os.path.splitext(mp3)
+                    new_file = base + '.mp3'
+                    os.rename(mp3, new_file)
+                    
+                    progMP4 = yt.streams.filter(progressive=True, file_extension='mp4')# 篩選 progressive 類型的 MP4 影片格式
+                    targetMP4 = progMP4.order_by('resolution').desc().first()# 找出解析度最好的 MP4 影片
+                    video_file= targetMP4.download('videos')
                 
-                progMP4 = yt.streams.filter(progressive=True, file_extension='mp4')# 篩選 progressive 類型的 MP4 影片格式
-                targetMP4 = progMP4.order_by('resolution').desc().first()# 找出解析度最好的 MP4 影片
-                video_file= targetMP4.download('videos')
+                except Exception as e:
+                    print(e)
+                    pass
+               
         print('播放清單下載完成')
           
-    except:
+    except Exception as e:
+        print(e)
         pass
     
 def Down_Image():
@@ -85,24 +125,27 @@ def Down_Image():
          try:
              url_req.urlretrieve(yt_image_url,filename)
              print('圖片下載完成')
-         except:
+         except Exception as e:
+             print(e)
              pass
          
-    except:
+    except Exception as e:
+        print(e)
         pass
     
     
     
 if __name__ == '__main__':
     while True:
-        act = input('請選擇模式:1(下載單支youtube) 2(下載清單) 3(下載縮圖)')
-        if act == '1':
-            Down_YT()
-        elif act == '2':
-            Down_List()
-        elif act == '3':
-            Down_Image()
-        else:
-            break
+        act = input('請選擇模式: 0:(EXIT) 1(下載單支youtube) 2(下載清單) 3(下載縮圖) :')
+        
+        if act == '0':break
+            
+        elif act == '1':Down_YT()
+        
+        elif act == '2':Down_List()
+        
+        elif act == '3':Down_Image()
+    
     
  
